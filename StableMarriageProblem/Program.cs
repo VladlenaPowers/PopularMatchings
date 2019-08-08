8ed0c0b2db237cefbca41e9ca6f664a42aabe259
Vladlena Powers <vladlenapowers@gmail.com>
2019-08-07 21:03:57 -0400

    New example

--- StableMarriageProblem/Program.cs
+++ StableMarriageProblem/Program.cs
@@ -313,7 +313,7 @@ namespace PopularMatching
         
         private static void DoTheThingCleanest()
         {
-            const int n = 3;
+            const int n = 2;
             var randomPrefLists = RandomPreferenceLists(n, 1);
 
             var sw = new Stopwatch();
@@ -349,21 +349,21 @@ namespace PopularMatching
 
                 //    return gsMaxIntersections[0] != gsMaxIntersections[1];
                 //})
-                .Where(pl =>
-                {
-                    var stableMatchingsWithoutGS = pl.StableMatchings
-                        .Where(m => !MatchingEqualityComparer.INSTANCE.Equals(pl.GaleShapelyMatching, m))
-                        .ToArray();
+                //.Where(pl =>
+                //{
+                //    var stableMatchingsWithoutGS = pl.StableMatchings
+                //        .Where(m => !MatchingEqualityComparer.INSTANCE.Equals(pl.GaleShapelyMatching, m))
+                //        .ToArray();
 
-                    var stableMaxIntersections = stableMatchingsWithoutGS
-                        .Select(k => pl.ParetoOptimalMatchings.MaxIntersections(k))
-                        .ToArray();
+                //    var stableMaxIntersections = stableMatchingsWithoutGS
+                //        .Select(k => pl.ParetoOptimalMatchings.MaxIntersections(k))
+                //        .ToArray();
 
-                    int maxValue = stableMaxIntersections.Max();
-                    int gsMaxValue = pl.ParetoOptimalMatchings.MaxIntersections(pl.GaleShapelyMatching);
+                //    int maxValue = stableMaxIntersections.Max();
+                //    int gsMaxValue = pl.ParetoOptimalMatchings.MaxIntersections(pl.GaleShapelyMatching);
 
-                    return gsMaxValue == maxValue;
-                })
+                //    return gsMaxValue == maxValue;
+                //})
                 .Select(x => x);
 
             foreach (var prefLists in passedPrefLists)
@@ -1094,38 +1094,78 @@ namespace PopularMatching
             //Testing();
             //return;
 
+        int[][] men = new int[16][] {
 
-        int[][] men = new int[12][] {
-        new int [5] { 6, 3, 2, 0, 1},
-        new int [5] { 8, 5, 0, 2, 1},
-        new int [5] { 7, 4, 2, 1, 0},
+        new int [] { 6, 2, 4, 0, 1},
+        new int [] { 7, 3, 5, 1, 0},
 
-        new int [5] { 6, 0, 5, 3, 4},
-        new int [5] { 8, 2, 3, 5, 4},
-        new int [5] { 7, 1, 5, 4, 3},
+        new int [] { 6, 0, 4, 2, 3},
+        new int [] { 7, 1, 5, 3, 2},
 
-        new int [5] { 0, 3, 8, 6, 7},
-        new int [5] { 2, 5, 6, 8, 7},
-        new int [5] { 1, 4, 8, 7, 6},
+        new int [] { 6, 0, 2, 4, 5},
+        new int [] { 7, 1, 3, 5, 4},
 
-        new int [1] { 1},
-        new int [1] { 1},
-        new int [1] { 1},
+        new int [] { 0, 2, 4, 6, 7},
+        new int [] { 1, 3, 5, 7, 6},
+
+        new int [] { 0},
+        new int [] { 2},
+        new int [] { 4},
+        new int [] { 6},
+
+        new int [] { 1},
+        new int [] { 3},
+        new int [] { 5},
+        new int [] { 7},
 };
 
-        int[][] women = new int[9][] {
-        new int [5] { 2, 0, 1, 3, 6},
-        new int [6] { 0, 1, 2, 9, 5, 8},
-        new int [5] { 1, 2, 0, 4, 7},
+        int[][] women = new int[8][] {
 
-        new int [5] { 5, 3, 4, 0, 6},
-        new int [6] { 3, 4, 5, 10, 2, 8},
-        new int [5] { 4, 5, 3, 1, 7},
+        new int [] { 1, 0, 8, 2, 4, 6},
+        new int [] { 0, 1, 12, 3, 5, 7},
 
-        new int [5] { 8, 6, 7, 0, 3},
-        new int [6] { 6, 7, 8, 11, 2, 5},
-        new int [5] { 7, 8, 6, 1, 4}
+        new int [] { 2, 3, 9,  0, 4, 6},
+        new int [] { 3, 2, 13, 1, 5, 7},
+
+        new int [] { 4, 5, 10,  0, 2, 6},
+        new int [] { 5, 4, 14, 1, 3, 7},
+
+        new int [] { 4, 5, 11, 0, 2, 4},
+        new int [] { 5, 4, 15, 1, 3, 5}
 };
+
+
+//            int[][] men = new int[12][] {
+//        new int [5] { 6, 3, 2, 0, 1},
+//        new int [5] { 8, 5, 0, 2, 1},
+//        new int [5] { 7, 4, 2, 1, 0},
+
+//        new int [5] { 6, 0, 5, 3, 4},
+//        new int [5] { 8, 2, 3, 5, 4},
+//        new int [5] { 7, 1, 5, 4, 3},
+
+//        new int [5] { 0, 3, 8, 6, 7},
+//        new int [5] { 2, 5, 6, 8, 7},
+//        new int [5] { 1, 4, 8, 7, 6},
+
+//        new int [1] { 1},
+//        new int [1] { 1},
+//        new int [1] { 1},
+//};
+
+//        int[][] women = new int[9][] {
+//        new int [5] { 2, 0, 1, 3, 6},
+//        new int [6] { 0, 1, 2, 9, 5, 8},
+//        new int [5] { 1, 2, 0, 4, 7},
+
+//        new int [5] { 5, 3, 4, 0, 6},
+//        new int [6] { 3, 4, 5, 10, 2, 8},
+//        new int [5] { 4, 5, 3, 1, 7},
+
+//        new int [5] { 8, 6, 7, 0, 3},
+//        new int [6] { 6, 7, 8, 11, 2, 5},
+//        new int [5] { 7, 8, 6, 1, 4}
+//};
 
 
 
